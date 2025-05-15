899a49d43a2a48f735a9e52c5c620dc84d26f069
Nicolas-AS07 <nico072312@gmail.com>
2025-05-15 10:38:14 -0300

    Conclusão desafio

---
+++ lab-banco-digital-oo-master/lab-banco-digital-oo-master/src/java/model/IConta.java
@@ -0,0 +1,9 @@
+// IConta.java
+package model;
+
+public interface IConta {
+	void sacar(double valor) throws IllegalArgumentException;
+	void depositar(double valor);
+	void transferir(double valor, IConta contaDestino) throws IllegalArgumentException;
+	void imprimirExtrato();
+}

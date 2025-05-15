11800e5948a10543996208852abd2c101726a87d
Nicolas-AS07 <nico072312@gmail.com>
2025-05-15 10:49:24 -0300

    Delete lab-banco-digital-oo-master directory

--- lab-banco-digital-oo-master/src/java/model/IConta.java
+++
@@ -1,9 +0,0 @@
-// IConta.java
-package model;
-
-public interface IConta {
-	void sacar(double valor) throws IllegalArgumentException;
-	void depositar(double valor);
-	void transferir(double valor, IConta contaDestino) throws IllegalArgumentException;
-	void imprimirExtrato();
-}

bfc795d4deb3b3f89d321d1192b7ee4b69c61d1f
Nicolas-AS07 <nico072312@gmail.com>
2025-05-15 10:40:21 -0300

    Conclusão desafio

---
+++ lab-banco-digital-oo-master/src/java/service/Banco.java
@@ -0,0 +1,42 @@
+// Banco.java
+package service;
+import model.Conta;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+	private String nome;
+	private List<Conta> contas;
+
+	public Banco(String nome) {
+		this.nome = nome;
+		this.contas = new ArrayList<>();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void adicionarConta(Conta conta) {
+		if (conta != null) {
+			contas.add(conta);
+		}
+	}
+
+	public void removerConta(Conta conta) {
+		contas.remove(conta);
+	}
+
+	public Conta buscarContaPorNumero(int numero) {
+		for (Conta conta : contas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		return null;
+	}
+
+	public List<Conta> getContas() {
+		return contas;
+	}
+}
